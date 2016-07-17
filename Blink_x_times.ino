02d6f930da8471f4cc7ee905d1e1cacee16fded7
Burathar <marijn-kuypers@hotmail.nl>
2016-07-18 01:22:56 +0200

    Update Blink_x_times.ino

--- Blink_x_times.ino
+++ Blink_x_times.ino
@@ -11,10 +11,10 @@ void loop() {
   Serial.setTimeout(3);
   if (Serial.available() > 0) {
     data = Serial.parseInt();
-    if (data == 2){
-    Serial.write("87653");
+    if (data == 2){     //The Raspberry Pi sends this vallue to initate the handshake, you can change it to whatever vallue you want as long as it is the same on the Raspberry side.
+    Serial.write("87653"); //This is the return vallue the Raspberry Pi waits for
     }
-    for (int i=0; i <= data - 1; i++){
+    for (int i=0; i <= data - 1; i++){  //This is just an optional debugging function
       digitalWrite(ledPin, HIGH);
       delay(200);
       digitalWrite(ledPin, LOW);
